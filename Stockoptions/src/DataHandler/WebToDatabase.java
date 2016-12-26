a8600cb33251040dfdae2ce7e228d4da241702c3
Randa1thor <craigsmailreplier@gmail.com>
2016-12-26 17:20:33 -0600

    Webtodatabase class

---
+++ Stockoptions/src/DataHandler/WebToDatabase.java
@@ -0,0 +1,13 @@
+package DataHandler;
+
+public class WebToDatabase {
+	
+	//needs logic to get from JSONs to Database structure
+	//use the regexparser to pull data from web response
+	//and insert into database
+	
+	
+	
+	
+
+}
