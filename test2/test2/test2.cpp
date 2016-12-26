3fa65574e6f602fd15a1c577dc4098c8f56f4962
That.Guy ... <henryarmage@gmail.com>
2016-12-26 13:05:22 -0600

    Testing

---
+++ test2/test2/test2.cpp
@@ -0,0 +1,17 @@
+// test2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << " Fuck it \n";
+
+	system("pause");
+    return 0;
+}
+
